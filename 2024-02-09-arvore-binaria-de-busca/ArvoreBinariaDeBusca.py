bfa4cab336aa3051cf3aaa1d0ed2df63ddd9baaf
KJ <kj.avelino.cambiaghi@gmail.com>
2024-05-13 21:55:40 -0300

    Arvore Binária de Busca

--- 2024-02-09-arvore-binaria-de-busca/ArvoreBinariaDeBusca.py
+++ 2024-02-09-arvore-binaria-de-busca/ArvoreBinariaDeBusca.py
@@ -11,7 +11,7 @@ class ArvoreBinariaDeBusca:
     def __init__(self) -> None:
         self.raiz: No | None = None
 
-    def insere(self, valor: int | str, raiz: No | None = None) -> No | None:
+    def insere(self, valor: int, raiz: No | None = None) -> No | None:
         if self.raiz is None:
             self.raiz  = No(valor)
             return self.raiz
@@ -27,6 +27,74 @@ class ArvoreBinariaDeBusca:
 
         return raiz
 
+    def busca(self, valor: int) -> No | None:
+        return self._busca(valor, self.raiz)
+
+    def _busca(self, valor: int, raiz: No | None) -> No | None:
+        if raiz is None or valor == raiz.valor:
+            return raiz
+        
+        if valor > raiz.valor:
+            return self._busca(valor, raiz.filhoDireito)
+
+        return self._busca(valor, raiz.filhoEsquerdo)
+
+    def remove(self, valor: int) -> None:
+        self.raiz = self._removeNo(valor, self.raiz)
+
+    def _removeNo(self, valor: int, raiz: No | None) -> No | None:
+        if raiz is None:
+            return raiz
+
+        if valor < raiz.valor:
+            raiz.filhoEsquerdo = self._removeNo(valor, raiz.filhoEsquerdo)
+            return raiz
+        
+        if valor > raiz.valor:
+            raiz.filhoDireito = self._removeNo(valor, raiz.filhoDireito)
+            return raiz
+
+        # raiz tem dois filhos
+        if raiz.filhoEsquerdo is not None and raiz.filhoDireito is not None:
+            self._trocaSucessor(raiz)
+            raiz.filhoDireito = self._removeNo(valor, raiz.filhoDireito)
+
+            return raiz
+
+        # raiz tem apenas um filho ou é folha 
+        if raiz.filhoEsquerdo is not None:
+            raiz = raiz.filhoEsquerdo
+            return raiz
+
+        raiz = raiz.filhoDireito
+        return raiz
+
+    def _trocaSucessor(self, no: No | None) -> None:
+        sucessor = no.filhoDireito
+        while (sucessor.filhoEsquerdo is not None):                
+            sucessor = sucessor.filhoEsquerdo
+            no.valor, sucessor.valor = sucessor.valor, no.valor
+
+    def altura(self, raiz: No | None) -> int:
+	    if raiz is None:
+	    	return -1
+
+	    alturaSubArvoreDireita = 1 + self.altura(raiz.filhoDireito)
+	    alturaSubArvoreEsquerda = 1 + self.altura(raiz.filhoEsquerdo)
+
+	    if alturaSubArvoreDireita > alturaSubArvoreEsquerda:
+		    return alturaSubArvoreEsquerda
+
+	    return alturaSubArvoreEsquerda
+
+    def percorrerEmInOrdem(self, raiz: No) -> None:
+	    if raiz == None:
+		    return None
+
+	    self.percorrerEmInOrdem(raiz.filhoEsquerdo)
+	    print(raiz.valor, ' ', end='')
+	    self.percorrerEmInOrdem(raiz.filhoDireito)
+
     def exibe(self, raiz: No | None) -> None:
         if self.raiz is None or raiz is None:
             return None
@@ -37,12 +105,28 @@ class ArvoreBinariaDeBusca:
         self.exibe(raiz.filhoDireito)
         print(")", end='')
 
-abb = ArvoreBinariaDeBusca()
-abb.insere(4, None)
-abb.insere(2, abb.raiz)
-abb.insere(3, abb.raiz)
-abb.insere(1, abb.raiz)
-abb.insere(6, abb.raiz)
-abb.insere(5, abb.raiz)
-abb.insere(7, abb.raiz)
-abb.exibe(abb.raiz)
+
+a = ArvoreBinariaDeBusca()
+a.insere(4)
+a.insere(2)
+a.insere(3)
+a.insere(1)
+a.insere(6)
+a.insere(5)
+a.insere(7)
+a.exibe(a.raiz)
+
+no = a.busca(7)
+if no != None:
+    print(no.valor)
+else: 
+    print(no)
+
+print('Altura da ABB a =', a.altura(a.raiz))
+print('Percurso da ABB a em in-ordem:')
+a.percorrerEmInOrdem(a.raiz)
+
+a.remove(7)
+a.exibe(a.raiz)
+a.remove(4)
+a.exibe(a.raiz)
